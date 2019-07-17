f10a0272227c304a2b018bfb9d07bb94d34a9dda
ggarg-c <ggarg-c@alierahealthcare.com>
2019-07-17 12:39:37 +0530

    bug fixes for cart crud

--- src/models/Book.js
+++ src/models/Book.js
@@ -5,7 +5,8 @@ const bookSchema = new mongoose.Schema({
     bookName:{type:String, required:true, unique:true},
     bookTitle:{type:String, required:true},
     author:{type:String, required:true},
-    genrees:{type:String, required:true}
+    genrees:{type:String, required:true},
+    price:{type:Number,required:true}
 },
 {timestamps:true});
 
--- src/routes/cart.js
+++ src/routes/cart.js
@@ -15,30 +15,72 @@ router.get('/getUserCartItems', (req, res) => {
                 if(err){
                     res.status(400).json({ errors: "unable to fetch cart items" })
                 } else{
-                    console.log(cartItems)
                     res.json(cartItems)
                 }
             });
         }
     });
 });
-router.get('/addToCart', (req, res) => {
+router.post('/addToCart', (req, res) => {
     const book = req.body.book;
-    const {userId} = req.body.user._id;
-    // Book.findOne({username}, (err, user) => {
-    //     if(err){
-    //         res.status(400).json({ errors: "Invalid User" })
-    //     } else{
-    //         Cart.find({userId:user._id},(err, cartItems) => {
-    //             if(err){
-    //                 res.status(400).json({ errors: "unable to fetch cart items" })
-    //             } else{
-    //                 console.log(cartItems)
-    //                 res.json(cartItems)
-    //             }
-    //         });
-    //     }
-    // });
+    const username = req.body.username;
+    User.findOne({username}, (err, user) => {
+        if(err){
+            res.status(400).json({ errors: "Invalid User" })
+        } else{
+            Cart.findOneAndUpdate({userId:user._id,bookId:book._id}, {$inc : {'quantity' : 1}},
+                { new: true }).then(cartItem => {
+                if(cartItem){
+                    Cart.find({userId:user._id},(err, cartItems) => {
+                        res.json({ success:true,cartItems });
+                    }); 
+                }
+                else{
+                    const item= new Cart({
+                        userId:user._id,
+                        bookId:book._id,
+                        bookName:book.bookName,
+                        quantity:1,
+                        pricePerUnit:book.price
+                    });
+                    item.save().then(res => {
+                        Cart.find({userId:user._id},(err, cartItems) => {
+                            res.json({ success:true,cartItems });
+                        }); 
+                    })
+                    .catch(err => {
+                        res.status(400).json({ errors:err.errors });
+                    });
+                }
+            });
+        }
+    });
 });
 
+router.post('/removeFromCart', (req, res) => {
+    const cartItem = req.body.cartItem;
+    Cart.findOneAndRemove({_id:cartItem._id}, (err, item) => {
+        if(!err){
+            Cart.find({userId:item.userId},(err, cartItems) => {
+                res.json({ success:true,cartItems });
+            }); 
+        } else{
+            res.status(400).json({ errors:err.errors });   
+        }
+    })
+});
+
+router.post('/updateQuantity', (req, res) => {
+    const cartItem = req.body.cartItem;
+    Cart.findOneAndUpdate({_id:cartItem._id}, { quantity: cartItem.quantity },
+        { new: true }).then(cartItem => {
+        if(cartItem){
+            Cart.find({userId:cartItem.userId},(err, cartItems) => {
+                res.json({ success:true,cartItems });
+            }); 
+        }
+        else{  res.status(400).json({ errors: err.errors });   
+        }
+    });
+});
 export default router;
